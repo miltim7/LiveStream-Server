c9d943393ee40679d3644f8f8652f31e1b57dcb7
miltim7 <timursailing70@gmail.com>
2024-10-03 21:36:16 +0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,41 @@
+// server.js
+const io = require('socket.io')(5000, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST'],
+    },
+});
+
+let broadcaster;
+
+io.on('connection', (socket) => {
+    console.log('Клиент подключился:', socket.id);
+
+    socket.on('broadcaster', () => {
+        broadcaster = socket.id;
+        socket.broadcast.emit('broadcaster');
+        console.log('Стример подключился:', broadcaster);
+    });
+
+    socket.on('watcher', () => {
+        console.log('Новый зритель:', socket.id);
+        socket.to(broadcaster).emit('watcher', socket.id);
+    });
+
+    socket.on('offer', (id, message) => {
+        socket.to(id).emit('offer', socket.id, message);
+    });
+
+    socket.on('answer', (id, message) => {
+        socket.to(id).emit('answer', socket.id, message);
+    });
+
+    socket.on('candidate', (id, message) => {
+        socket.to(id).emit('candidate', socket.id, message);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Клиент отключился:', socket.id);
+        socket.to(broadcaster).emit('disconnectPeer', socket.id);
+    });
+});
